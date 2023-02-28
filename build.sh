f72671dcb6fac80601f3533942334422e676d43f
dan25g <dangar452000@gmail.com>
2023-02-28 14:26:05 -0400

    new mod4

--- build.sh
+++ build.sh
@@ -4,7 +4,7 @@ set -o errexit
 
 pip install gitpython
 pip install --upgrade pip
-pip install --upgrade setuptools
+pip install setuptools
 pip install -r requirements.txt git
 
 python manage.py collectstatic --no-input
