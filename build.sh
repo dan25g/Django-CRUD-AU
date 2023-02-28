4a1b821d475a31d6f83cdfaa8dc400b04e16b674
dan25g <dangar452000@gmail.com>
2023-02-28 14:23:07 -0400

    new mod3

--- build.sh
+++ build.sh
@@ -2,10 +2,9 @@
 # exit on error
 set -o errexit
 
-
-pip install --upgrade pip
-#pip install --upgrade setuptools
 pip install gitpython
+pip install --upgrade pip
+pip install --upgrade setuptools
 pip install -r requirements.txt git
 
 python manage.py collectstatic --no-input
